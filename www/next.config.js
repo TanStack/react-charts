7c42a71d91e19bc1613448fc7b6a11f9f4f1e20f
Tanner Linsley <tannerlinsley@gmail.com>
2019-11-19 13:14:39 -0700

    Update next.config.js

--- www/next.config.js
+++ www/next.config.js
@@ -1,4 +1,4 @@
-const Module = require('module')
+// const Module = require('module')
 const path = require('path')
 const withCSS = require('@zeit/next-css')
 const withImages = require('next-images')
@@ -6,33 +6,33 @@ const resolveFrom = require('resolve-from')
 
 const node_modules = path.resolve(__dirname, 'node_modules')
 
-const originalRequire = Module.prototype.require
+// const originalRequire = Module.prototype.require
 
-// The following ensures that there is always only a single (and same)
-// copy of React in an app at any given moment.
-Module.prototype.require = function(modulePath) {
-  // Only redirect resolutions to non-relative and non-absolute modules
-  if (['/react/', '/react-dom/'].some(d => {
-    try {
-      return require.resolve(modulePath).includes(d)
-    } catch (err) {
-      return false
-    }
-  })) {
-    console.log(modulePath)
-    try {
-      modulePath = resolveFrom(
-        node_modules,
-        modulePath
-      )
-      console.log('New: ', modulePath)
-    } catch (err) {
-      //
-    }
-  }
+// // The following ensures that there is always only a single (and same)
+// // copy of React in an app at any given moment.
+// Module.prototype.require = function(modulePath) {
+//   // Only redirect resolutions to non-relative and non-absolute modules
+//   if (['/react/', '/react-dom/'].some(d => {
+//     try {
+//       return require.resolve(modulePath).includes(d)
+//     } catch (err) {
+//       return false
+//     }
+//   })) {
+//     console.log(modulePath)
+//     try {
+//       modulePath = resolveFrom(
+//         node_modules,
+//         modulePath
+//       )
+//       console.log('New: ', modulePath)
+//     } catch (err) {
+//       //
+//     }
+//   }
 
-  return originalRequire.call(this, modulePath)
-}
+//   return originalRequire.call(this, modulePath)
+// }
 
 const baseConfig = {
   target: 'serverless',
