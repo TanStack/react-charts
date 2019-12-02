ed67b7e554c1649f0a6915424dadad658c70f27c
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2019-12-02 17:42:05 +0530

    setup size-plugin-bot

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
 import resolve from "rollup-plugin-node-resolve";
+import size from 'rollup-plugin-size';
 
 import pkg from "./package.json";
 
@@ -25,6 +26,12 @@ export default {
       exclude: "node_modules/**"
     }),
     resolve(),
-    commonjs()
+    commonjs(),
+    size({
+      publish: true,
+      exclude: pkg.module,
+      filename: 'sizes-cjs.json',
+      writeFile: process.env.CI ? true : false,
+    })
   ]
 };
