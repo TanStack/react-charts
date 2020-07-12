0b5acbbd328ad9225ab79d11ddcdbbacca8b5eb4
Henry Hsu <henry@qlane.com>
2020-07-12 13:38:50 -0400

    Prettier format.

--- src/seriesTypes/Pie.js
+++ src/seriesTypes/Pie.js
@@ -1,17 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext } from 'react'
 
-import ChartContext from "../utils/ChartContext";
-import Utils from "../utils/Utils";
+import ChartContext from '../utils/ChartContext'
+import Utils from '../utils/Utils'
 
 //
-import Path from "../primitives/Path";
+import Path from '../primitives/Path'
 
 export default function Pie({ series }) {
-  const [{ focused, primaryAxes }] = useContext(ChartContext);
+  const [{ focused, primaryAxes }] = useContext(ChartContext)
 
-  const style = series.getStatusStyle(Utils.getStatus(series, focused));
+  const style = series.getStatusStyle(Utils.getStatus(series, focused))
 
-  const primaryAxis = primaryAxes[0];
+  const primaryAxis = primaryAxes[0]
 
   return (
     <g
@@ -19,11 +19,11 @@ export default function Pie({ series }) {
         transform: Utils.translate(
           primaryAxis.width / 2,
           primaryAxis.height / 2
-        )
+        ),
       }}
     >
       {series.datums.map((datum, i) => {
-        const dataStyle = datum.getStatusStyle(Utils.getStatus(datum, focused));
+        const dataStyle = datum.getStatusStyle(Utils.getStatus(datum, focused))
 
         return (
           <Path
@@ -33,18 +33,18 @@ export default function Pie({ series }) {
               ...style,
               ...style.arc,
               ...dataStyle,
-              ...dataStyle.arc
+              ...dataStyle.arc,
             }}
           />
-        );
+        )
       })}
     </g>
-  );
+  )
 }
 
 Pie.defaultProps = {
-  showPoints: true
-};
+  showPoints: true,
+}
 
 Pie.plotDatum = (datum, { primaryAxis }) => {
   // Decorate the datum with the scale info
@@ -52,35 +52,35 @@ Pie.plotDatum = (datum, { primaryAxis }) => {
     ...datum,
     ...primaryAxis.scale(datum),
     defined:
-      Utils.isValidPoint(datum.xValue) && Utils.isValidPoint(datum.yValue)
-  };
+      Utils.isValidPoint(datum.xValue) && Utils.isValidPoint(datum.yValue),
+  }
 
   // Set the anchor point
-  datum.anchor = { x: datum.x, y: datum.y };
+  datum.anchor = { x: datum.x, y: datum.y }
 
   // Set the pointer points (used in voronoi)
-  datum.boundingPoints = [datum.anchor];
+  datum.boundingPoints = [datum.anchor]
 
   // Return the new datum
-  return datum;
-};
+  return datum
+}
 
 Pie.buildStyles = (
   series,
   { getSeriesStyle, getDatumStyle, defaultColors }
 ) => {
   series.getStatusStyle = status => {
-    series.style = Utils.getStatusStyle(series, status, getSeriesStyle);
-    return series.style;
-  };
+    series.style = Utils.getStatusStyle(series, status, getSeriesStyle)
+    return series.style
+  }
 
   // We also need to decorate each datum in the same fashion
   series.datums.forEach(datum => {
     datum.getStatusStyle = status => {
       datum.style = Utils.getStatusStyle(datum, status, getDatumStyle, {
-        color: defaultColors[datum.index % (defaultColors.length - 1)]
-      });
-      return datum.style;
-    };
-  });
-};
+        color: defaultColors[datum.index % (defaultColors.length - 1)],
+      })
+      return datum.style
+    }
+  })
+}
