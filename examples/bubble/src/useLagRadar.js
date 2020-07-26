cc449184af9d09d55f78ffd2e95c046851d409e1
Tanner Linsley <tannerlinsley@gmail.com>
2020-07-25 20:47:30 -0600

    docs: responsive lag radar

--- examples/bubble/src/useLagRadar.js
+++ examples/bubble/src/useLagRadar.js
@@ -1,17 +1,19 @@
 import React from "react";
 
+import { useWindowSize } from "@react-hook/window-size";
+
 export default function useLagRadar() {
-  React.useEffect(
-    () =>
-      lagRadar({
-        frames: 60, // number of frames to draw, more = worse performance
-        speed: 0.0017, // how fast the sweep moves (rads per ms)
-        size: 300, // outer frame px
-        inset: 3, // circle inset px
-        parent: document.body // DOM node to attach to
-      }),
-    []
-  );
+  const [width, height] = useWindowSize();
+
+  React.useEffect(() => {
+    return lagRadar({
+      frames: 60, // number of frames to draw, more = worse performance
+      speed: 0.0017, // how fast the sweep moves (rads per ms)
+      size: Math.min(width, height) / 3, // outer frame px
+      inset: 3, // circle inset px
+      parent: document.body // DOM node to attach to
+    });
+  }, [width, height]);
 }
 
 /**
