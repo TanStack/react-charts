d0f84bee72da53a58de03b91d083294bf86d406f
Tanner Linsley <tannerlinsley@gmail.com>
2022-08-05 13:13:24 -0600

    Update useRect.ts

--- src/hooks/useRect.ts
+++ src/hooks/useRect.ts
@@ -64,30 +64,5 @@ export default function useRect(
     }
   }, [element, enabled, measure, rerender])
 
-  // const resolvedRect = React.useMemo(() => {
-  //   if (!element || !(element as Element).tagName) {
-  //     return rect
-  //   }
-
-  //   const styles = window.getComputedStyle(element as Element)
-
-  //   return {
-  //     x: rect.x,
-  //     y: rect.y,
-  //     width:
-  //       rect.width -
-  //       parseInt(styles.borderLeftWidth) -
-  //       parseInt(styles.borderRightWidth),
-  //     height:
-  //       rect.height -
-  //       parseInt(styles.borderTopWidth) -
-  //       parseInt(styles.borderBottomWidth),
-  //     top: rect.top,
-  //     right: rect.right,
-  //     bottom: rect.bottom,
-  //     left: rect.left,
-  //   }
-  // }, [element, rect])
-
   return rectRef.current
 }
